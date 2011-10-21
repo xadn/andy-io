ff001db4abac590a36b4b7b86d857cbc39eb5f23
Andy Niccolai <andy@andy.io>
2011-10-20 23:16:01 -0600

    added styling

--- web.js
+++ web.js
@@ -2,11 +2,24 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	response.render('index.jade', { locals: {
+		title: 'andy.io'
+		}
+	});
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
