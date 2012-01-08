eef20e260916e0740a72a9cbd15ba7286d411247
Andy <andy.niccolai@gmail.com>
2012-01-08 14:47:02 -0700

    moved require stuff around

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,28 @@
 define(
-	['jquery',
-	'models/localcursor', 'models/cursor-collection', 'views/localcursor', 'views/cursor-collection',
+	['models/localcursor', 'models/cursor-collection', 'views/localcursor', 'views/cursor-collection',
 	'models/message-collection', 'views/message-collection', 'views/message',
 	'/socket.io/socket.io.js'],
-	function($, LocalCursor, CursorCollection, LocalCursorView, CursorCollectionView,
+	function(LocalCursor, CursorCollection, LocalCursorView, CursorCollectionView,
 		MessageCollection, MessageCollectionView, MessageView){
 
 		return {
 			initialize: function() {
 
+				// Models
+
 				var localCursor = new LocalCursor();
-
 				var cursorCollection = new CursorCollection();
-
-				var localCursorView = new LocalCursorView({model: localCursor});
-
-				var cursorCollectionView = new CursorCollectionView({collection: cursorCollection});
-
 				var messageCollection = new MessageCollection();
 
+				// Views
+
+				var localCursorView = new LocalCursorView({model: localCursor});
+				var cursorCollectionView = new CursorCollectionView({collection: cursorCollection});
 				var messageCollectionView = new MessageCollectionView({collection: messageCollection});
 
-				var socket = io.connect();
+				// Socket.io handlers
 
+				var socket = io.connect();
 
 				socket.on('updateCursor', cursorCollection.updateData);
 
---
+++ public/scripts/require-config.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+	require.config({
+		paths: {
+			models: 	'app/models',
+			views: 		'app/views',
+			routers: 	'app/routers',
+			jquery: 	'libs/jquery',
+			underscore: 'libs/underscore',
+			backbone: 	'libs/backbone',
+			domready: 	'libs/domReady'
+		}
+	});
+}());
